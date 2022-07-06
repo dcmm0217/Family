758feb1329418bd814742399f6ce13cd9f520556
HGooV威 <huangwei6750@qq.com>
2022-07-06 23:51:15 +0800

    es demo

---
+++ ElasticSearch/myes/src/test/java/com/wei/myes/MyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.myes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
