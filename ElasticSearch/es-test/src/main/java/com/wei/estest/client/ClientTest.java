1cf64140f22b870fc37ee400f6eb0962aa5a3f0c
huangw <huangwei6750@qq.com>
2022-07-17 23:17:23 +0800

    es-测试环境

---
+++ ElasticSearch/es-test/src/main/java/com/wei/estest/client/ClientTest.java
@@ -0,0 +1,22 @@
+package com.wei.estest.client;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+/**
+ * @author huangw
+ * @date 2022/7/17 14:02
+ */
+public class ClientTest {
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        client.close();
+    }
+}
